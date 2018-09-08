06448c1f18f770e9e54dd40f0ead868c50859ad1
xiaowu-git <2503677254@qq.com>
2018-09-08 17:13:28 +0800

    Initial commit

---
+++ javaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/9/8
+  Time: 10:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>javaweb</title>
+  </head>
+  <body>
+  <h1>hello idea!!!</h1>
+  </body>
+</html>
---
+++ src/demo/HelloDemo.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FileName: HelloDemo
+ * Author:   Administrator
+ * Date:     2018/9/8 7:46
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class HelloDemo {
+    public static void main(String[] args){
+
+        int a = 13;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+
+}
